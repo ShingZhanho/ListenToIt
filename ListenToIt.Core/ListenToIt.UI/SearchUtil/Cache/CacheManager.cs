09101507f3f147a8ecf504081de3f0ce2c9623fe
Zhanho Shing <zhanhoshing@icloud.com>
2021-01-28 07:31:15 +0800

    Create CacheManager class
    Created a class for cache file managerment

---
+++ ListenToIt.Core/ListenToIt.UI/SearchUtil/Cache/CacheManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListenToIt.UI.SearchUtil.Cache {
+    /// <summary>
+    /// A class for managing cached audio files.
+    /// </summary>
+    internal class CacheManager {
+        /// <summary>
+        /// Creates a CacheManager instance without cache list, cache list will be created.
+        /// </summary>
+        public CacheManager() {
+            
+        }
+        
+        /// <summary>
+        /// Creates a CacheManager instance with an existing cache list.
+        /// </summary>
+        /// <param name="cacheList">Path of the cache list file.</param>
+        public CacheManager(string cacheList) {
+            
+        }
+    }
+}
