a428238586c85b5cd9e02ea3915c27cfe61547e1
Zhanho Shing <zhanhoshing@icloud.com>
2021-02-02 14:26:05 +0800

    Added code for comparing versions

--- ListenToIt.Core/ListenToIt.Updater/Program.cs
+++ ListenToIt.Core/ListenToIt.Updater/Program.cs
@@ -6,8 +6,12 @@ using ListenToIt.Updater.CmdOptions;
 namespace ListenToIt.Updater {
     public static partial class Program {
         static void Main(string[] args) {
+            var arguments = args;
+            // Uncomment the following lines for debugging
+            // arguments = "DEBUG PARAMETERS".Split(' ');
+            
             // Parses command line options
-            Parser.Default.ParseArguments<UpdateOptions>(args)
+            Parser.Default.ParseArguments<UpdateOptions>(arguments)
                 .WithParsed(CheckUpdate)
                 .WithNotParsed(ErrorParsingArgs);
         }
@@ -15,6 +19,8 @@ namespace ListenToIt.Updater {
         private static void CheckUpdate(UpdateOptions options) {
             Console.WriteLine("\"check\" command received");
             var update = new UpdateDownloader(options);
+            if (options.CheckOnly)
+                Environment.Exit(update.IsUpToDate() ? 0 : 200);
         }
 
         private static void ErrorParsingArgs(IEnumerable<Error> errors) {
--- ListenToIt.Core/ListenToIt.Updater/UpdateDownloader.cs
+++ ListenToIt.Core/ListenToIt.Updater/UpdateDownloader.cs
@@ -6,13 +6,16 @@ using Octokit;
 namespace ListenToIt.Updater {
     public partial class Program {
         private class UpdateDownloader {
-            private UpdateOptions _options; // Options fetched from command line
+            private readonly UpdateOptions _options; // Options fetched from command line
             
             // Information fetched from server
             private Release _latestRelease;
+            private Version _latestVersion;
+            private readonly Version _currentVersion;
 
             public UpdateDownloader(UpdateOptions options) {
                 _options = options;
+                _currentVersion = new Version(options.CurrentVersion);
                 FetchFromServer();
             }
 
@@ -27,12 +30,50 @@ namespace ListenToIt.Updater {
                 foreach (var release in releases) {
                     if (!_options.IncludePrerelease) if (release.Prerelease) continue; // Skip for beta versions
                     _latestRelease = release;
+                    _latestVersion = new Version(release.TagName);
                 }
             }
 
             public bool IsUpToDate() {
-                return true;
+                return !_latestVersion.IsNewerThan(_currentVersion);
             }
+        }
+
+        public class Version {
+            public Version(int major, int minor, int patch, int revision, VersionSuffix suffix) {
+                Major = major;
+                Minor = minor;
+                Patch = patch;
+                Revision = revision;
+                Suffix = suffix;
+            }
+
+            /// <summary>
+            /// Construct a Version instance.
+            /// </summary>
+            /// <param name="rawVersion">rawVersion should follow "Major.Minor.Patch.Revision-Suffix" format.</param>
+            public Version(string rawVersion) : this (
+                Convert.ToInt32(rawVersion.Split('-')[0].Split('.')[0]),
+                Convert.ToInt32(rawVersion.Split('-')[0].Split('.')[1]),
+                Convert.ToInt32(rawVersion.Split('-')[0].Split('.')[2]),
+                Convert.ToInt32(rawVersion.Split('-')[0].Split('.')[3]),
+                rawVersion.Split('-')[1] == "stable" ? VersionSuffix.Stable : VersionSuffix.Beta
+                ){}
+            
+            public int Major { get; }
+            public int Minor { get; }
+            public int Patch { get; }
+            public int Revision { get; }
+            public VersionSuffix Suffix { get; }
+
+            public bool IsNewerThan(Version ver) {
+                if (Major > ver.Major) return true;
+                if (Minor > ver.Major) return true;
+                if (Patch > ver.Patch) return true;
+                return Revision > ver.Revision;
+            }
+
+            public enum VersionSuffix { Stable, Beta }
         }
     }
 }
