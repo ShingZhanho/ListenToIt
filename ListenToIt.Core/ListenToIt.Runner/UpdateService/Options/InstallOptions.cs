8dfae44989a7b700058aad52d73c03f6f6a6d226
Zhanho Shing <zhanhoshing@icloud.com>
2021-02-05 22:17:46 +0800

    feat: finished coding of InstallOptions class

--- ListenToIt.Core/ListenToIt.Runner/UpdateService/Options/InstallOptions.cs
+++ ListenToIt.Core/ListenToIt.Runner/UpdateService/Options/InstallOptions.cs
@@ -1,7 +1,18 @@
-﻿namespace ListenToIt.Runner.UpdateService.Options {
+﻿using System.IO;
+
+namespace ListenToIt.Runner.UpdateService.Options {
     public sealed class InstallOptions : BaseOptions {
+        public string InstallDir { get; set; }
+        public string PackagePath { get; set; }
+        public bool RemoveAfterInstall { get; set; }
         public override string GetCmdArgs() {
-            return null;
+            var args = string.Empty;
+
+            args = $"{args} -d {Path.GetFullPath(InstallDir)}"; // set -d argument
+            args = $"{args} -p {Path.GetFullPath(PackagePath)}"; // set -p argument
+            args = RemoveAfterInstall ? $"{args} -r" : args; // set -r flag
+            
+            return args;
         }
     }
 }
