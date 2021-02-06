00f0c4490c3a67cba7e80ed83a1629b0583a2e10
Zhanho Shing <zhanhoshing@icloud.com>
2021-02-07 00:21:17 +0800

    feat: start UI from runner The runner can now look for the UI executable and launch it.

--- ListenToIt.Core/ListenToIt.Runner/Program.cs
+++ ListenToIt.Core/ListenToIt.Runner/Program.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
+using ListenToIt.Runner.UpdateService;
 
 namespace ListenToIt.Runner {
-    static class Program {
-        static void Main() {
-            
+    public static class Program {
+        public static void Main() {
+            // Look for the latest version, and start the app
+            var exePath = string.Empty;
+            foreach (var directory in Directory.GetDirectories(Application.StartupPath)) {
+                if (!Version.IsValidRawVersionString(directory)) continue;
+                exePath = Path.Combine(directory, "ListenToIt.App.exe");
+            }
+            var appProcess = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = exePath
+                }
+            };
+            appProcess.Start();
+            appProcess.WaitForExit();
         }
     }
 }
