49c2e8a1c67025fd2d5e33cee4868cb423be2612
Zhanho Shing <zhanhoshing@icloud.com>
2021-02-07 00:52:13 +0800

    feat: add methods for checking updates

--- ListenToIt.Core/ListenToIt.Runner/UpdateService/Updater.cs
+++ ListenToIt.Core/ListenToIt.Runner/UpdateService/Updater.cs
@@ -1,5 +1,24 @@
-﻿namespace ListenToIt.Runner.UpdateService {
-    public class Updater {
-        
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using ListenToIt.Runner.UpdateService.Options;
+
+namespace ListenToIt.Runner.UpdateService {
+    public static class Updater {
+        public static int CheckUpdate(CheckOptions options) => StartUpdater(options.GetCmdArgs());
+
+        private static int StartUpdater(string args) {
+            var suProcess = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = Path.Combine(Application.StartupPath, "SU\\ListenToIt.Updater.exe"),
+                    Arguments = args,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            suProcess.Start();
+            suProcess.WaitForExit();
+            return suProcess.ExitCode;
+        }
     }
 }
